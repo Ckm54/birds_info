a95e27831e9c3fcec09f5c42a75ab4cb55db6520
collo <mutaic84@gmail.com>
2022-11-08 18:13:55 +0300

    enable serving static files

--- main.go
+++ main.go
@@ -11,6 +11,12 @@ import (
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", handler).Methods("GET")
+
+	staticFileDirectory := http.Dir("./assets")
+
+	staticFileHandler := http.StripPrefix("/assets/" ,http.FileServer(staticFileDirectory))
+
+	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
 	return r
 }
 
