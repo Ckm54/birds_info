825e75da021badbdc1455b8e812244e0c05f2663
collo <mutaic84@gmail.com>
2022-11-08 18:10:50 +0300

    add more test cases

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -27,5 +28,65 @@ func TestHandler(t *testing.T) {
 	actual := recorder.Body.String()
 	if actual != expected {
 		t.Errorf("handler returned unexpected body: got %v expected %v", actual, expected)
+	}
+}
+
+// Test routing
+func TestRouter(t *testing.T) {
+	r := newRouter()
+	mockServer := httptest.NewServer(r)
+
+	resp, err := http.Get(mockServer.URL + "/hello")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Status should be ok, got %d", resp.StatusCode)
+	}
+
+	// read response body and convert to a string
+	defer resp.Body.Close()
+	// read the body into a bunch of bytes
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	respString := string(b)
+	expected := "Hello World!"
+
+	if respString != expected {
+		t.Errorf("Response body should be %s, got %s", expected, respString)
+	}
+}
+
+func TestRouterForNonExistentRoute(t *testing.T) {
+	r := newRouter()
+	mockServer := httptest.NewServer(r)
+
+	resp, err := http.Post(mockServer.URL + "/hello", "", nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// expected status code 405: Method not allowed
+	if resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf("Status should be 405, got %d", resp.StatusCode)
+	}
+
+	// test expect an empty body
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	respString := string(b)
+	expected := ""
+
+	if respString != expected {
+		t.Errorf("Response should be %s, got %s", expected, respString)
 	}
 }
