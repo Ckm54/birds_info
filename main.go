8a5ffa69a4aa7133184377d6778dfebba112dde1
collo <mutaic84@gmail.com>
2022-11-08 22:07:51 +0300

    create handler to fetch birds

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
 	"github.com/gorilla/mux"
 )
+
+type Bird struct {
+	Species			string	`json:"species"`
+	Description	string	`json:"description"`
+}
+
+var birds []Bird
 
 // creates router and returns so we can test the
 // router outside the main function
@@ -28,4 +37,18 @@ func main() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
+}
+
+func getBirdHandler(w http.ResponseWriter, r *http.Request) {
+	birdListBytes, err := json.Marshal(birds)
+
+	// if error print to console and return server error to the user
+	if err != nil {
+		fmt.Println(fmt.Errorf("error: %v", err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// else write json list of birds to the response
+	w.Write(birdListBytes)
 }
